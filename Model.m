013a8d5d144cdb36d7c86b116eb75b60ebc4d471
Romero-Prieto <julio.romero-prieto@lshtm.ac.uk>
2024-06-27 10:28:15 +0100

    Model.m

--- Model.m
+++ Model.m
@@ -1,43 +1,20 @@
 function [maTCh,q,La,E,MSE] = Model(B,xo,x,SET,match,lambda)
-ReG     = B{2};
-B       = B{1};
 tables  = size(xo,1);
-Z       = table2array(ReG(1,5));
-lAB     = x{2};
-x       = x{1};
+Z       = table2array(B{2}(1,5));
 X       = [xo(:,1).^(0:Z),xo(:,2:end)];
 order   = Z + size(xo,2);
-B       = B(:,1:order);
+fx      = exp(B{1}(:,1:order)*X');
 
-if ~iscell(B)
-    ages = size(B,1);
-    B    = reshape(B',[numel(B) 1]);
-    X    = kron(eye(ages),X);
-    fx   = reshape(min(exp(X*B),1),[tables ages])';
-    clear X B
-else
-    y  = 0;
-    for i = 1:size(X,2)
-        y = y + B{i}.*X(:,i)';
-    end
-    fx   = exp(y);
-    clear y X B
-end
-
-if isequal(char(table2array(ReG(1,4))),'q')
-    l = 1 - [zeros(1,tables);fx];
-    d = [l(1:end-1,:) - l(2:end,:);l(end,:)];
-    l = cumsum(d,'reverse');
-    q = 1 - l./l(1,:);
-elseif isequal(char(table2array(ReG(1,4))),'m')
+if isequal(char(table2array(B{2}(1,4))),'q')
+    q = [zeros(1,tables);fx]
+elseif isequal(char(table2array(B{2}(1,4))),'m')
     m = fx;
-    n = x(2:end) - x(1:end-1);
+    n = x{1}(2:end) - x{1}(1:end-1);
     q = 1 - [ones(1,tables);cumprod(exp(-m.*n),1)];
 end
  
-[maTCh] = Set(q,{x,lAB},SET,xo);
-w       = SET{1};
-w       = table2array(w(:,4));
+[maTCh] = Set(q,x,SET,xo);
+w       = table2array(SET{1}(:,4));
 w       = w/sum(w);
 
 E       = recode(log(match{1}./maTCh{1}),[-inf,inf],[0;0])*sqrt(w);
